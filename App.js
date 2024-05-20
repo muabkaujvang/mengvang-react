e2ca3f317e5393d02e1d6ba7b03c4b7f88848e30
muabkauj <ltub530@gmail.com>
2024-05-20 11:34:31 +0700

    react commit

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View,Image, ScrollView } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is Meng's react native</Text>
       <StatusBar style="auto" />
+      <Image
+        source={{
+          uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEeb2nTx_hgEhnuGpAl7hK8khnk9k2JA4F6pX7XK6eeA&s",
+        }}
+        width={300}
+        height={350}
+      ></Image>
+
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.text}>Item 1</Text>
+        <Text style={styles.text}>Item 2</Text>
+        <Text style={styles.text}>Item 3</Text>
+        <Text style={styles.text}>Item 1</Text>
+        <Text style={styles.text}>Item 2</Text>
+        <Text style={styles.text}>Item 3</Text>
+        <Text style={styles.text}>Item 1</Text>
+        <Text style={styles.text}>Item 2</Text>
+        <Text style={styles.text}>Item 3</Text>
+        <Text style={styles.text}>Item 1</Text>
+        <Text style={styles.text}>Item 2</Text>
+        <Text style={styles.text}>Item 3</Text>
+        <Text style={styles.text}>Item 1</Text>
+        <Text style={styles.text}>Item 2</Text>
+        <Text style={styles.text}>Item 3</Text>
+        <Text style={styles.text}>Item 1</Text>
+        <Text style={styles.text}>Item 2</Text>
+        <Text style={styles.text}>Item 3</Text>
+      </ScrollView>
+
+      <TextInput
+        autoCorrect={true}
+        placeholder="Type here"
+        value="this.state.value"
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 60,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+    color: "#000", // Black text color
+  },
+  input: {
+    height: 40,
+    borderColor: "#333",
+    borderWidth: 1,
+    paddingHorizontal: 8,
   },
 });
